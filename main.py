f57b77f2463d2ecd28f0e031b8b30a4798670f31
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-07-31 11:44:07 +0300

    Update main.py
    Removed the passwords to avoid exploitation

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ app.secret_key = '1a2b3c4d5e'
 
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '33812040'
+app.config['MYSQL_USER'] = '****'
+app.config['MYSQL_PASSWORD'] = '*******'
 app.config['MYSQL_DB'] = 'pythonlogin'
 
 # Intialize MySQL
