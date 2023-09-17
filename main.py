d11c36d352e4251eece1e39bd09c59c1242814fe
HarunMbaabu <mbaabuharn8@gmail.com>
2023-09-17 18:33:33 +0300

    Configure and update the python packages

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.secret_key = '1a2b3c4d5e6d7g8h9i10'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '*****' #Replace ******* with  your database password.
+app.config['MYSQL_PASSWORD'] = '*********' #Replace ******* with  your database password.
 app.config['MYSQL_DB'] = 'loginapp'
 
 
